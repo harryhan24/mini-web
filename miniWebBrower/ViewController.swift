1eee9df1c3549f0e692e564275096270bbfe114f
harryhan24 <herryhan2435@gmail.com>
2016-12-05 03:34:20 +0900

    okok

--- miniWebBrower/ViewController.swift
+++ miniWebBrower/ViewController.swift
@@ -8,13 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//텍스트 필드에서 생기는 이벤트들을 UITextFieldDelegate가 처리해준다.
+class ViewController: UIViewController, UITextFieldDelegate, UIWebViewDelegate {
 
+    @IBOutlet weak var bookMarkSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var mainWebView: UIWebView!
+    
+    @IBOutlet weak var spinningActivity: UIActivityIndicatorView!
+    
+    @IBAction func goBackButton(_ sender: Any) {
+        mainWebView.goBack()
+    }
+    @IBAction func goForwardButton(_ sender: Any) {
+        mainWebView.goForward()
+    }
+    
+    
+
+    @IBAction func bookMarkAction(_ sender: Any) {
+        
+    
+        let bookMarkUrl = bookMarkSegmentedControl.titleForSegment(at: bookMarkSegmentedControl.selectedSegmentIndex)!
+        let urlString = "https://www.\(bookMarkUrl).com"
+        mainWebView.loadRequest(URLRequest(url:URL(string:urlString)!));
+        urlTextField.text = urlString;
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        var urlString = "\(urlTextField.text!)"
+        if !urlString.hasPrefix("https;//"){
+            urlString = "https://\(urlTextField.text!)"
+        }
+        mainWebView.loadRequest(URLRequest(url:URL(string:urlString)!));
+        textField.resignFirstResponder();
+        return true
+    }
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        spinningActivity.startAnimating()
+        let urlString = "https://www.facebook.com"
+        //url 로드
+        mainWebView.loadRequest(URLRequest(url:URL(string:urlString)!));
+        urlTextField.text = urlString;
+//        spinningActivity.stopAnimating()
     }
 
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        spinningActivity.startAnimating()
+    }
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        spinningActivity.stopAnimating()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
